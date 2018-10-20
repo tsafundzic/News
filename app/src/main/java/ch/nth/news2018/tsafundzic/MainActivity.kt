650ae153be4e4620887e8f8550241748f3607e66
Tomislav <tsafundzic@gmail.com>
2018-10-20 10:37:50 +0200

    Initial commit

---
+++ app/src/main/java/ch/nth/news2018/tsafundzic/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.nth.news2018.tsafundzic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
